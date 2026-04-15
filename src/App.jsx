af3221beedb8a387a2f8f83633e0f6eb5bca33bc
Misko <m.hresko.dev@gmail.com>
2026-04-15 13:45:14 +0200

    added scanner

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import ProgressManager from "./tools/ProgressManager"
-import QrCodeTranslator from "./tools/QrCodeTranslator"
+import ProgressManager from "./tools/ProgressManager";
+import QrCodeTranslator from "./tools/QrCodeTranslator";
+import { Scanner } from "@yudiel/react-qr-scanner";
 
 export default function App() {
-
   // Progress Manager
   let options = {
-    valueKey: 'player:progress'
-  }
-  let progressManager = new ProgressManager(options)
+    valueKey: "player:progress",
+  };
+  let progressManager = new ProgressManager(options);
 
   // QR Translator
-  let qrCodeTranslator = new QrCodeTranslator()
+  let qrCodeTranslator = new QrCodeTranslator();
 
   return (
     <>
-      <h1 className='text-4xl'>Test</h1>
+      <Scanner
+        onScan={(result) => console.log(result)}
+        onError={(error) => console.log(error?.message)}
+      />
     </>
-  )
+  );
 }
